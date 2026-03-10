0af1c6257043cf7a96dee0bf1095233856d8b183
Maryfik <francuzovamasa@gmail.com>
2026-03-10 11:21:15 +0300

    Exclude .vscode folder from tracking

--- 08_lesson/test_projects.py
+++ 08_lesson/test_projects.py
@@ -1,30 +1,6 @@
-import pytest
 import requests
 
 # POST
-
-
-@pytest.fixture
-def base_url():
-    return " "
-
-
-@pytest.fixture
-def my_payload():
-    return {
-        "title": "ГосУслуги2",
-        "users": {
-            "5cfc4bde-600f-4c21-8260-80d87b72f5ec": "admin"
-        }
-    }
-
-
-@pytest.fixture
-def my_headers():
-    return {
-        "Content-Type": "application/json",
-        "Authorization": " "
-    }
 
 
 def test_create_project_positive(base_url, my_payload, my_headers):
